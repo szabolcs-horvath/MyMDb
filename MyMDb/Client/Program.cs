a4697be7271dfd0309d1d62b956d4e3c8018eaab
szabolcs-horvath <szaby0927@gmail.com>
2022-05-14 21:38:59 +0200

    Modified Snackbar position in global config

--- MyMDb/Client/Program.cs
+++ MyMDb/Client/Program.cs
@@ -8,6 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+});
 
 await builder.Build().RunAsync();
