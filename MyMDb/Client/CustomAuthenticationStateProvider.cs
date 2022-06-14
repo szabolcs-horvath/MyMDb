7d8b160771ce088554f1c4fb509bcd7291fb546c
szabolcs-horvath <szaby0927@gmail.com>
2022-06-15 00:19:20 +0200

    Primitive functional login page

--- MyMDb/Client/CustomAuthenticationStateProvider.cs
+++ MyMDb/Client/CustomAuthenticationStateProvider.cs
@@ -19,7 +19,7 @@ namespace MyMDb.Client
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             //string token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVG9ueSBTdGFyayIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6Iklyb24gTWFuIiwiZXhwIjozMTY4NTQwMDAwfQ.IbVQa1lNYYOzwso69xYfsMOHnQfO3VLvVqV2SOXS7sTtyyZ8DEf5jmmwz2FGLJJvZnQKZuieHnmHkg7CGkDbvA";
-            var token = await _localStorage.GetItemAsStringAsync("token");
+            var token = (await _localStorage.GetItemAsStringAsync("token"))?.Replace("\"", "");
 
             var identity = new ClaimsIdentity(); //Unauthorized
             _http.DefaultRequestHeaders.Authorization = null; //Unauthorized
@@ -27,7 +27,7 @@ namespace MyMDb.Client
             if (!string.IsNullOrEmpty(token))
             {
                 identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt"); //Authorized
-                _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Replace("\"", ""));
+                _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             }
 
             var user = new ClaimsPrincipal(identity);
