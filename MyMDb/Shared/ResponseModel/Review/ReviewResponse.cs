220bd7ebfb8c72fc9021358dae43ed89d3f41d04
szabolcs-horvath <szaby0927@gmail.com>
2022-06-20 20:24:17 +0200

    Removed default values

--- MyMDb/Shared/ResponseModel/Review/ReviewResponse.cs
+++ MyMDb/Shared/ResponseModel/Review/ReviewResponse.cs
@@ -8,8 +8,8 @@ namespace MyMDb.Shared.ResponseModel.Review
         public int Id { get; set; }
         public int MovieId { get; set; }
         public int MyMDbUserId { get; set; }
-        public string Headline { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string Headline { get; set; }
+        public string Description { get; set; }
         public bool Spoiler { get; set; }
         public MovieBasicResponse? Movie { get; set; }
         public MyMDbUserBasicResponse? MyMDbUser { get; set; }
