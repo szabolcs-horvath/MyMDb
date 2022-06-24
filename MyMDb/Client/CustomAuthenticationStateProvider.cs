031bab502462f807ce22a294c39f869329ade5e8
szabolcs-horvath <szaby0927@gmail.com>
2022-06-24 19:36:44 +0200

    Added check for JWT expiration date, but only with mock value (always valid)

--- MyMDb/Client/CustomAuthenticationStateProvider.cs
+++ MyMDb/Client/CustomAuthenticationStateProvider.cs
@@ -21,13 +21,22 @@ namespace MyMDb.Client
             //string token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVG9ueSBTdGFyayIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6Iklyb24gTWFuIiwiZXhwIjozMTY4NTQwMDAwfQ.IbVQa1lNYYOzwso69xYfsMOHnQfO3VLvVqV2SOXS7sTtyyZ8DEf5jmmwz2FGLJJvZnQKZuieHnmHkg7CGkDbvA";
             var token = (await _localStorage.GetItemAsStringAsync("token"))?.Replace("\"", "");
 
-            var identity = new ClaimsIdentity(); //Unauthorized
+            var identity = new ClaimsIdentity();
             _http.DefaultRequestHeaders.Authorization = null; //Unauthorized
 
             if (!string.IsNullOrEmpty(token))
             {
-                identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt"); //Authorized
-                _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var expiryDate = GetExpiryTimestampFromJwt(token);
+
+                if (DateTime.Compare(DateTime.Now, expiryDate) < 0)
+                {
+                    identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+                    _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token); //Authorized
+                }
+                else
+                {
+                    await _localStorage.RemoveItemAsync("token");
+                }
             }
 
             var user = new ClaimsPrincipal(identity);
@@ -36,6 +45,12 @@ namespace MyMDb.Client
             NotifyAuthenticationStateChanged(Task.FromResult(state));
 
             return state;
+        }
+
+        private static DateTime GetExpiryTimestampFromJwt(string token)
+        {
+            //TODO
+            return DateTime.Now.AddHours(6);
         }
 
         private static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
